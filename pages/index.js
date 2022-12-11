cac7cdd9c46fedc06ed765368c33da17913fcf0e
Recep Kefeli <105538090+recepkefelii@users.noreply.github.com>
2022-12-11 14:58:47 +0300

    add portfolio project nextjs

---
+++ pages/index.js
@@ -0,0 +1,63 @@
+import Head from "next/head";
+import {
+  AiFillLinkedin,
+  AiFillGithub
+} from "react-icons/ai";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { useState } from "react";
+import deved from "../public/dev-ed-wave.png";
+import Image from "next/image";
+
+
+export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  return (
+    <div className={darkMode ? "dark" : ""}>
+      <Head>
+        <title>Recep Kefeli</title>
+      </Head>
+      <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between dark:text-white">
+            <h1 className="font-burtons text-xl"></h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill
+                  onClick={() => setDarkMode(!darkMode)}
+                  className=" cursor-pointer text-2xl"
+                />
+              </li>
+              <li>
+              </li>
+            </ul>
+          </nav>
+          <div className="text-center p-10 py-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
+             Recep Kefeli
+            </h2>
+            <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
+              Fullstack Developer
+            </h3>
+            <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
+            I am actively developing full stack websites
+            </p>
+            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
+              <a href="https://github.com/recepkefelii">
+              <AiFillGithub />
+              </a>
+              <a href="https://www.linkedin.com/in/recep-kefeli-606338244/">
+                <AiFillLinkedin/>
+              </a>
+            </div>
+            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+              <Image src={deved} layout="fill" objectFit="cover" />
+            </div>
+          </div>
+        </section>
+        <section>
+        </section>
+      </main>
+    </div>
+  );
+}
